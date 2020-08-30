d6a62e6ecab1a35e116802dd470fcbb5508aefda
daiywlf@gmail.com <daiywlf5251@gmail.com>
2020-08-31 02:14:15 +0900

    fix link of sss , add home page, back ground image

--- project/SSS.py
+++ project/SSS.py
@@ -1,11 +1,7 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from pymongo import MongoClient
-
-client = MongoClient()
-db = client.Board_DB
-collection = db.SSS_Collection
-
+import re
 def extract_smarts():
     url = "https://e-onestop.pusan.ac.kr/menu/bbs/notice/list?menuId=20001104&rMenu=12"
     options = webdriver.ChromeOptions()
@@ -25,8 +21,12 @@ def extract_smarts():
 
         title = datas[1].a.get_text().strip()
         rate = datas[2].get_text().strip().replace('-', '.')
-        link = '#'
+        link_index = re.findall("\\d+",datas[1].a['onclick'])[0]
+        link = f"https://e-onestop.pusan.ac.kr/menu/bbs/notice/view?bbsConfNo=1&articleNo={link_index}&menuId=20001104&rMenu=12"
         smart = {'title': title, 'rate': rate, 'link': link}
+        client = MongoClient()
+        db = client.Board_DB
+        collection = db.SSS_Collection
         collection.update(smart,smart,upsert=True)
 
         smarts.append(
